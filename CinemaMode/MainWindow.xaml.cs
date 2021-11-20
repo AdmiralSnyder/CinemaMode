64515e6157927b6e08f33ebac77c45cd57f0bd6f
Alexander Gayko <a.gayko@googlemail.com>
2021-11-20 02:15:02 +0100

    Add project files.

---
+++ CinemaMode/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿using Alex.PInvoke;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfScreenHelper;
+
+namespace CinemaMode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            WindowRectange = new();
+
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+            this.LocationChanged += MainWindow_LocationChanged;
+            this.SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e) => ApplyCurrentSize();
+
+        private void MainWindow_LocationChanged(object? sender, EventArgs e) => ApplyCurrentLocation();
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (var window in Windows)
+            {
+                window.Close();
+            }
+        }
+
+        private readonly Rectangle WindowRectange;
+
+        private void ApplyCurrentLocation()
+        {
+            WindowRectange.Visibility = Visibility.Collapsed;
+            Canvas.SetLeft(WindowRectange, Left / 10 + VirtualScreenWidthHalfScaled + 1);
+            Canvas.SetTop(WindowRectange, Top / 10 + VirtualScreenHeightHalfScaled + 1);
+            WindowRectange.Visibility = Visibility.Visible;
+        }
+
+        private void ApplyCurrentSize()
+        {
+            WindowRectange.Width = Width / 10 - 2;
+            WindowRectange.Height = Height / 10 - 2;
+        }
+
+        private double VirtualScreenWidthHalfScaled;
+        private double VirtualScreenHeightHalfScaled;
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            int margin = 1;
+            VirtualScreenWidthHalfScaled = SystemParameters.VirtualScreenWidth / 20;
+            VirtualScreenHeightHalfScaled = SystemParameters.VirtualScreenHeight / 20;
+
+            int i = 0;
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                Rectangle r = new();
+                r.RadiusX = 10;
+                r.RadiusY = 10;
+                Canvas.SetLeft(r, screen.Bounds.Left / 10 + VirtualScreenWidthHalfScaled + margin);
+                Canvas.SetTop(r, screen.Bounds.Top / 10 + VirtualScreenHeightHalfScaled + margin);
+                r.Width = screen.Bounds.Width / 10 - 2 * margin;
+                r.Height = screen.Bounds.Height / 10 - 2 * margin;
+                
+                r.InputBindings.Add(new MouseBinding(new MyCommand<Shape>(DisableAllOtherScreens, r), new(MouseAction.LeftClick)));
+                r.Tag = screen.DeviceName;
+                r.Fill = Brushes.Green;
+                r.Stroke = Brushes.Red;
+                r.Visibility = Visibility.Visible;
+
+                Canvas.Children.Add(r);
+            }
+
+            WindowRectange.RadiusX = 10;
+            WindowRectange.RadiusY = 10;
+            WindowRectange.Fill = Brushes.White;
+            WindowRectange.Stroke = Brushes.Black;
+            WindowRectange.Visibility = Visibility.Visible;
+            ApplyCurrentLocation();
+            ApplyCurrentSize();
+            Canvas.Children.Add(WindowRectange);
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                TextBlock textBlock = new();
+                textBlock.Text = (++i).ToString();// screen.DeviceName.Split("DISPLAY")[1];
+                textBlock.Width = 30;
+                textBlock.Height = 15;
+                textBlock.TextAlignment = TextAlignment.Center;
+                textBlock.Visibility = Visibility.Visible;
+                textBlock.FontWeight = FontWeights.UltraBold;
+
+                Canvas.Children.Add(textBlock);
+                Canvas.SetLeft(textBlock, screen.Bounds.Left / 10 + VirtualScreenWidthHalfScaled + screen.Bounds.Width / 20 - textBlock.Width / 2);
+                Canvas.SetTop(textBlock, screen.Bounds.Top / 10 + VirtualScreenHeightHalfScaled + screen.Bounds.Height / 20 - textBlock.Height / 2);
+            }
+
+        }
+
+        //private void DisableAllOtherScreens(Shape shape) => shape.Fill = shape.Fill == Brushes.Yellow ? Brushes.Black : Brushes.Yellow;
+
+        private void DisableAllOtherScreens(Shape shape)
+        {
+            foreach (var screen in WpfScreenHelper.Screen.AllScreens.ToList())
+            {
+                var deviceName = (string)shape.Tag;
+                if (screen.DeviceName != deviceName)
+                {
+                    DisableScreen(screen);
+                }
+            }
+        }
+
+        const int DM_POSITION = 0x20;
+
+        const int DM_PELSWIDTH = 0x80000;
+        const int DM_PELSHEIGHT = 0x100000;
+
+        private List<Window> Windows = new();
+
+
+        private void DisableScreen(Screen screen)
+        {
+            var screenWin = new Window();
+            Windows.Add(screenWin);
+            screenWin.Background = Brushes.Black;
+            screenWin.Width = screen.Bounds.Width;
+            screenWin.Height = screen.Bounds.Height;
+            screenWin.Left = screen.Bounds.Left;
+            screenWin.Top = screen.Bounds.Top;
+            screenWin.Topmost = true;
+            screenWin.BorderBrush = Brushes.Black;
+            screenWin.BorderThickness = new(0);
+            screenWin.ShowInTaskbar = false;
+            screenWin.WindowStyle = WindowStyle.None;
+            screenWin.ResizeMode = ResizeMode.NoResize;
+            //screenWin.bord
+            screenWin.Show();
+        }
+
+        private void DisableScreenByAttaching(string deviceName)
+        {
+            DEVMODE devMode = new();
+            devMode.dmSize = (short)Marshal.SizeOf<DEVMODE>();
+            devMode.dmDriverExtra = 0;
+            devMode.dmFields = DM_POSITION | DM_PELSHEIGHT | DM_PELSWIDTH;
+            devMode.dmPelsWidth = 0;
+            devMode.dmPelsHeight = 0;
+
+            //POINTL delete = new();
+            //delete.x = 0;
+            //delete.y = 0;
+            //devMode.dmPosition = delete;
+
+            DEVMODE devModeZero = new();
+
+            //A pointer to a DEVMODE structure that describes the new graphics mode.If lpDevMode is NULL, all the values currently in the registry will be used for the display setting.Passing NULL for the lpDevMode parameter and 0 for the dwFlags parameter is the easiest way to return to the default mode after a dynamic mode change.
+            DEVMODE dmTemp;
+            var res = User32.EnumDisplaySettings(deviceName, ENUM_IMODENUM.ENUM_REGISTRY_SETTINGS, out dmTemp);
+
+            dmTemp.dmFields = DM_POSITION | DM_PELSHEIGHT | DM_PELSWIDTH;
+            dmTemp.dmPelsWidth = 0;
+            dmTemp.dmPelsHeight = 0;
+
+
+            User32.ChangeDisplaySettingsEx(deviceName, ref dmTemp, IntPtr.Zero, ChangeDisplaySettingsFlags.CDS_UPDATEREGISTRY | ChangeDisplaySettingsFlags.CDS_NORESET, IntPtr.Zero);
+
+            User32.ChangeDisplaySettingsEx(null, ref devModeZero, IntPtr.Zero, 0, IntPtr.Zero);
+            //ChangeDisplaySettingsEX
+            //PInvoke.User32.changedis
+        }
+
+        private class MyCommand<T> : ICommand
+        {
+            public MyCommand(Action<T> action, T parameter) => (Action, Parameter) = (action, parameter);
+
+            public Action<T> Action { get; }
+            public T Parameter { get; }
+
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter) => true;
+
+            public void Execute(object? parameter)
+            {
+                Action(Parameter);
+            }
+        }
+    }
+}
---
+++ CinemaMode/PInvoke.cs
@@ -0,0 +1,206 @@
+﻿using PInvoke;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alex.PInvoke
+{
+    internal class User32
+    {
+        //ChangeDisplaySettingsEX
+        [DllImport("user32.dll")]
+        public static extern DISP_CHANGE ChangeDisplaySettingsEx(string lpszDeviceName, ref DEVMODE lpDevMode, IntPtr hwnd, ChangeDisplaySettingsFlags dwflags, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string lpszDeviceName, ENUM_IMODENUM iModeNum, out DEVMODE lpDevMode);
+    }
+
+    public enum ENUM_IMODENUM : int
+    {
+        NUM_CURRENT_SETTINGS = -1,
+        ENUM_REGISTRY_SETTINGS = -2,
+    }
+
+    public enum DISP_CHANGE : int
+    {
+        Successful = 0,
+        Restart = 1,
+        Failed = -1,
+        BadMode = -2,
+        NotUpdated = -3,
+        BadFlags = -4,
+        BadParam = -5,
+        BadDualView = -6
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct DEVMODE
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmDeviceName;
+        public short dmSpecVersion;
+        public short dmDriverVersion;
+        public short dmSize;
+        public short dmDriverExtra;
+        public int dmFields;
+        public short dmOrientation;
+        public short dmPaperSize;
+        public short dmPaperLength;
+        public short dmPaperWidth;
+        public short dmScale;
+        public short dmCopies;
+        public short dmDefaultSource;
+        public short dmPrintQuality;
+        public short dmColor;
+        public short dmDuplex;
+        public short dmYResolution;
+        public short dmTTOption;
+        public short dmCollate;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmFormName;
+        public short dmUnusedPadding;
+        public short dmBitsPerPel;
+        public int dmPelsWidth;
+        public int dmPelsHeight;
+        public int dmDisplayFlags;
+        public int dmDisplayFrequency;
+    }
+
+    //[StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi)]
+    //public struct DEVMODE
+    //{
+    //    public const int CCHDEVICENAME = 32;
+    //    public const int CCHFORMNAME = 32;
+
+    //    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+    //    [System.Runtime.InteropServices.FieldOffset(0)]
+    //    public string dmDeviceName;
+    //    [System.Runtime.InteropServices.FieldOffset(32)]
+    //    public Int16 dmSpecVersion;
+    //    [System.Runtime.InteropServices.FieldOffset(34)]
+    //    public Int16 dmDriverVersion;
+    //    [System.Runtime.InteropServices.FieldOffset(36)]
+    //    public Int16 dmSize;
+    //    [System.Runtime.InteropServices.FieldOffset(38)]
+    //    public Int16 dmDriverExtra;
+    //    [System.Runtime.InteropServices.FieldOffset(40)]
+    //    public /*DM*/int dmFields;
+
+    //    [System.Runtime.InteropServices.FieldOffset(44)]
+    //    Int16 dmOrientation;
+    //    [System.Runtime.InteropServices.FieldOffset(46)]
+    //    Int16 dmPaperSize;
+    //    [System.Runtime.InteropServices.FieldOffset(48)]
+    //    Int16 dmPaperLength;
+    //    [System.Runtime.InteropServices.FieldOffset(50)]
+    //    Int16 dmPaperWidth;
+    //    [System.Runtime.InteropServices.FieldOffset(52)]
+    //    Int16 dmScale;
+    //    [System.Runtime.InteropServices.FieldOffset(54)]
+    //    Int16 dmCopies;
+    //    [System.Runtime.InteropServices.FieldOffset(56)]
+    //    Int16 dmDefaultSource;
+    //    [System.Runtime.InteropServices.FieldOffset(58)]
+    //    Int16 dmPrintQuality;
+
+    //    [System.Runtime.InteropServices.FieldOffset(44)]
+    //    public POINTL dmPosition;
+    //    [System.Runtime.InteropServices.FieldOffset(52)]
+    //    public Int32 dmDisplayOrientation;
+    //    [System.Runtime.InteropServices.FieldOffset(56)]
+    //    public Int32 dmDisplayFixedOutput;
+
+    //    [System.Runtime.InteropServices.FieldOffset(60)]
+    //    public short dmColor; // See note below!
+    //    [System.Runtime.InteropServices.FieldOffset(62)]
+    //    public short dmDuplex; // See note below!
+    //    [System.Runtime.InteropServices.FieldOffset(64)]
+    //    public short dmYResolution;
+    //    [System.Runtime.InteropServices.FieldOffset(66)]
+    //    public short dmTTOption;
+    //    [System.Runtime.InteropServices.FieldOffset(68)]
+    //    public short dmCollate; // See note below!
+    //    [System.Runtime.InteropServices.FieldOffset(70)]
+    //    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+    //    public string dmFormName;
+    //    [System.Runtime.InteropServices.FieldOffset(102)]
+    //    public Int16 dmLogPixels;
+    //    [System.Runtime.InteropServices.FieldOffset(104)]
+    //    public Int32 dmBitsPerPel;
+    //    [System.Runtime.InteropServices.FieldOffset(108)]
+    //    public Int32 dmPelsWidth;
+    //    [System.Runtime.InteropServices.FieldOffset(112)]
+    //    public Int32 dmPelsHeight;
+    //    [System.Runtime.InteropServices.FieldOffset(116)]
+    //    public Int32 dmDisplayFlags;
+    //    [System.Runtime.InteropServices.FieldOffset(116)]
+    //    public Int32 dmNup;
+    //    [System.Runtime.InteropServices.FieldOffset(120)]
+    //    public Int32 dmDisplayFrequency;
+    //}
+
+    public struct POINTL
+    {
+        public Int32 x;
+        public Int32 y;
+    }
+
+    /// <summary>
+    /// Selects duplex or double-sided printing for printers capable of duplex printing.
+    /// </summary>
+    public enum DMDUP : short
+    {
+        /// <summary>
+        /// Unknown setting.
+        /// </summary>
+        DMDUP_UNKNOWN = 0,
+
+        /// <summary>
+        /// Normal (nonduplex) printing.
+        /// </summary>
+        DMDUP_SIMPLEX = 1,
+
+        /// <summary>
+        /// Long-edge binding, that is, the long edge of the page is vertical.
+        /// </summary>
+        DMDUP_VERTICAL = 2,
+
+        /// <summary>
+        /// Short-edge binding, that is, the long edge of the page is horizontal.
+        /// </summary>
+        DMDUP_HORIZONTAL = 3,
+    }
+
+    /// <summary>
+    /// Switches between color and monochrome on color printers.
+    /// </summary>
+    public enum DMCOLOR : short
+    {
+        DMCOLOR_UNKNOWN = 0,
+
+        DMCOLOR_MONOCHROME = 1,
+
+        DMCOLOR_COLOR = 2
+    }
+
+    [Flags()]
+    public enum ChangeDisplaySettingsFlags : uint
+    {
+        CDS_NONE = 0,
+        CDS_UPDATEREGISTRY = 0x00000001,
+        CDS_TEST = 0x00000002,
+        CDS_FULLSCREEN = 0x00000004,
+        CDS_GLOBAL = 0x00000008,
+        CDS_SET_PRIMARY = 0x00000010,
+        CDS_VIDEOPARAMETERS = 0x00000020,
+        CDS_ENABLE_UNSAFE_MODES = 0x00000100,
+        CDS_DISABLE_UNSAFE_MODES = 0x00000200,
+        CDS_RESET = 0x40000000,
+        CDS_RESET_EX = 0x20000000,
+        CDS_NORESET = 0x10000000
+    }
+
+}
